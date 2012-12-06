e467885b89ff25f54cb0837050be3e81217ec2f9
Yang Yang <geraint0923@gmail.com>
2012-12-06 16:14:14 +0800

    The new project could take photo now.

--- leChecker/src/com/msra/elechecker/MainActivity.java
+++ leChecker/src/com/msra/elechecker/MainActivity.java
@@ -1,22 +1,124 @@
 package com.msra.elechecker;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements SurfaceHolder.Callback {
 
+	private SurfaceView previewSurface = null;
+	private SurfaceHolder previewHolder = null;
+	
+	private SurfaceView photoSurface = null;
+	private SurfaceHolder photoHolder = null;
+	
+	private Camera camera = null;
+	
+	private Button photoButton = null;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		camera = Camera.open();
+		
+		previewSurface = (SurfaceView) this.findViewById(R.id.preview_surface);
+		
+		photoButton = (Button) this.findViewById(R.id.photo_button);
+		
+		photoButton.setOnClickListener(new Button.OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				if(camera != null) {
+					camera.takePicture(null, null, pictureCallback);
+				}
+			}
+			
+		});
+		
+		previewHolder = previewSurface.getHolder();
+		previewHolder.addCallback(this);
+		previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+		
 	}
+	
+	private PictureCallback pictureCallback = new Camera.PictureCallback() {
+		
+		@Override
+		public void onPictureTaken(byte[] arg0, Camera arg1) {
+			// TODO Auto-generated method stub
+			System.out.println("***********************I have take a photo!!!!!**************size:"+ arg0);
+			camera.startPreview();
+			
+			
+			File file = new File("/sdcard/mypic.jpeg");
+			if(!file.exists())
+				try {
+					file.createNewFile();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			try {
+				FileOutputStream fos = new FileOutputStream(file);
+				fos.write(arg0);
+				fos.close();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+	};
+	
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
+		// TODO Auto-generated method stub
+		camera.startPreview();
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder arg0) {
+		// TODO Auto-generated method stub
+		try {
+			camera.setPreviewDisplay(previewHolder);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
