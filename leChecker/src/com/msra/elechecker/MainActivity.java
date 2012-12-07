2f800345c1befa35728507395b8f3c23dd751c03
Yang Yang <geraint0923@gmail.com>
2012-12-07 18:04:12 +0800

    Can convert a bitmap to a grey image.

--- leChecker/src/com/msra/elechecker/MainActivity.java
+++ leChecker/src/com/msra/elechecker/MainActivity.java
@@ -6,6 +6,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
@@ -29,6 +30,9 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
 	private SurfaceView photoSurface = null;
 	private SurfaceHolder photoHolder = null;
 	
+	private SurfaceView processSurface = null;
+	private SurfaceHolder processHolder = null;
+	
 	private Camera camera = null;
 	
 	private Button photoButton = null;
@@ -45,6 +49,9 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
 		
 		previewSurface = (SurfaceView) this.findViewById(R.id.preview_surface);
 		photoSurface = (SurfaceView) this.findViewById(R.id.photo_surface);
+		processSurface = (SurfaceView) this.findViewById(R.id.process_surface);
+		
+		processHolder = processSurface.getHolder();
 		
 		photoButton = (Button) this.findViewById(R.id.photo_button);
 		
@@ -66,6 +73,7 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
 		
 		
 		photoHolder = photoSurface.getHolder();
+		
 		
 	}
 	
@@ -109,6 +117,8 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
 			
 			System.out.println("=================I have take a photo!!!!!===================size:"+ arg0);
 			
+			
+			// Lock the photo canvas
 			Canvas canvas = photoHolder.lockCanvas();
 			
 			Paint paint = new Paint();
@@ -123,6 +133,9 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
 			
 			//bitmap = Bitmap.createScaledBitmap(bitmap, canvas.getWidth(), canvas.getHeight(), true);
 			
+			int[] pixs = new int[bitmap.getWidth() * bitmap.getHeight()];
+			bitmap.getPixels(pixs, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+			
 			
 			canvas.drawBitmap(bitmap, 0, 0, paint);
 			
@@ -130,6 +143,13 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
 			photoHolder.unlockCanvasAndPost(canvas);
 			//Log.e("println", "scale height and width:"+ sh + ", "+sw);
 			
+			
+			// Lock the process canvas
+			canvas = processHolder.lockCanvas();
+			paint = new Paint();
+			bitmap = convertGreyImg(bitmap);
+			canvas.drawBitmap(bitmap, 0, 0, paint);
+			processHolder.unlockCanvasAndPost(canvas);
 		}
 	};
 	
@@ -186,4 +206,32 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
         super.onPause();  
     } 
 
+    
+    public Bitmap convertGreyImg(Bitmap img) {
+    	int width = img.getWidth();
+    	int height = img.getHeight();
+
+    	int []pixels = new int[width * height];
+
+    	img.getPixels(pixels, 0, width, 0, 0, width, height);
+    	int alpha = 0xFF << 24;
+    	for(int i = 0; i < height; i++) {
+	    	for(int j = 0; j < width; j++) {
+		    	int grey = pixels[width * i + j];
+		
+		    	int red = ((grey & 0x00FF0000 ) >> 16);
+		    	int green = ((grey & 0x0000FF00) >> 8);
+		    	int blue = (grey & 0x000000FF);
+		
+		    	grey = (int)((float) red * 0.3 + (float)green * 0.59 + (float)blue * 0.11);
+		    	grey = alpha | (grey << 16) | (grey << 8) | grey;
+		    	pixels[width * i + j] = grey;
+	    	}
+    	}
+    	Bitmap result = Bitmap.createBitmap(width, height, Config.RGB_565);
+    	result.setPixels(pixels, 0, width, 0, 0, width, height);
+    	return result;
+    } 
+    
+    
 }
