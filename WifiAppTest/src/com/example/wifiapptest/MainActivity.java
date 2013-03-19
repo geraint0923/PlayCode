16e2a6a74b9c539333c04f8d347c5930b5f3790d
Yang Yang <geraint0923@gmail.com>
2013-03-20 01:49:05 +0800

    Create the whole framework of the app

--- WifiAppTest/src/com/example/wifiapptest/MainActivity.java
+++ WifiAppTest/src/com/example/wifiapptest/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.wifiapptest;
 
+import android.R.integer;
+import java.net.*;
+import android.R.integer;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -18,5 +21,87 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
+	
+	private void setCountText(String str) {
+		
+	}
+	
+	private void setTriggerText(String str) {
+		
+	}
+	
+	private void setBatchText(String str) {
+		
+	}
 
+	private TriggerTester triggerTester = null;
+	private BatchTester batchTester = null;
+	
+	private final int PORT = 2323;
+	
+	private DatagramSocket socket = null;
+	private DatagramPacket packet = null;
+	
+	private void initSocket(int port) {
+		
+	}
+	
+	abstract class Tester {
+		protected boolean isRunning = false;
+		
+		public abstract void start(int count, int para);
+		
+		public abstract void stop();
+		
+		public abstract void onRecv();
+	}
+	
+	class TriggerTester extends Tester {
+
+		
+		@Override
+		public void start(int count, int para) {
+			// TODO Auto-generated method stub
+			// we also need the time of delay
+		}
+
+		@Override
+		public void stop() {
+			// TODO Auto-generated method stub
+			if(!isRunning) {
+				
+			}
+		}
+
+		@Override
+		public void onRecv() {
+			// TODO Auto-generated method stub
+			// 
+		}
+
+	}
+	
+	class BatchTester extends Tester {
+
+		@Override
+		public void start(int count, int para) {
+			// TODO Auto-generated method stub
+			// we also need the batch size
+		}
+
+		@Override
+		public void stop() {
+			// TODO Auto-generated method stub
+			if(!isRunning) {
+				
+			}
+		}
+
+		@Override
+		public void onRecv() {
+			// TODO Auto-generated method stub
+			// Dummy method, don't do any thing
+		}
+
+	}
 }
