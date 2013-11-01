f9a1f2a40fb6e5d5b371ea9d39196ad293105c2b
Yang Yang <geraint0923@gmail.com>
2013-11-01 21:23:19 +0800

    Update code

--- ReportServer/app/controllers/log_report_controller.rb
+++ ReportServer/app/controllers/log_report_controller.rb
@@ -4,10 +4,13 @@ class LogReportController < ApplicationController
 	def report
 		puts "FILE FINE LOG!"
 		puts params.keys
-		ss = params[:log_report].to_json
-		f = File.open(Rails.root.to_s + "/db/log/#{params[:REPORT_ID]}.log", 'w')
-		f.puts ss
-		f.close
+#		ss = params[:log_report].to_json
+#		f = File.open(Rails.root.to_s + "/db/log/#{params[:REPORT_ID]}.log", 'w')
+#		f.puts ss
+#		f.close
+#		pp params[:log_report]
+#		mongo_client = MongoClient.new
+#		puts mongo_client.database_names
 		render :json => {
 			'result' => 'OK'
 		}.to_json
