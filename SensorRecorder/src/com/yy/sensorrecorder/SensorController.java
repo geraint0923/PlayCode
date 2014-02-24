f19107ea00a1ad70c77894e823c49d8c90a9a635
Yang Yang <geraint0923@gmail.com>
2014-02-24 19:33:56 +0800

    Just a checkpoint

---
+++ SensorRecorder/src/com/yy/sensorrecorder/SensorController.java
@@ -0,0 +1,26 @@
+package com.yy.sensorrecorder;
+
+import java.io.OutputStream;
+
+
+
+
+public class SensorController {
+	
+	private OutputStream outputStream;
+	public SensorController() {
+		
+	}
+	
+	public void setOutputStream(OutputStream stream) {
+		
+	}
+	
+	public void startSensors() {
+		
+	}
+	
+	public void stopSensors() {
+		
+	}
+}
--- SensorRecorder/src/com/yy/sensorrecorder/SensorRecorderActivity.java
+++ SensorRecorder/src/com/yy/sensorrecorder/SensorRecorderActivity.java
@@ -3,8 +3,9 @@ package com.yy.sensorrecorder;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.SurfaceHolder;
 
-public class SensorRecorderActivity extends Activity {
+public class SensorRecorderActivity extends Activity implements SurfaceHolder.Callback {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +18,24 @@ public class SensorRecorderActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.sensor_recorder, menu);
 		return true;
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
